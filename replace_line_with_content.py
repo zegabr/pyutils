6c95ef417355e53c1759c6ead41466444eccd65c
zegabr <jgsp2@cin.ufpe.br>
2020-11-04 17:54:05 -0300

    add comment tag arg

--- replace_line_with_content.py
+++ replace_line_with_content.py
@@ -1,8 +1,11 @@
 """
-this script recieves 3 arguments: 
+this script recieves 4 arguments: 
     F the file to be changed,
     CONTENT the content of the line that must be changed
-    NEW_CONTENT the content to replace the line that has CONTENT     
+    NEW_CONTENT the content to replace the line that has CONTENT
+    COMMENT_TAG the comment symbol to ignore     
+
+you can use this to add line to a file or change a line that is already there
 """
 
 import sys
@@ -10,6 +13,7 @@ import sys
 FILE = sys.argv[1]
 CONTENT = sys.argv[2]
 NEW_CONTENT = sys.argv[3]
+COMMENT_TAG = sys.argv[4]
 
 file_obj = open(FILE, 'r')
 lines = file_obj.read().split('\n')
@@ -18,7 +22,7 @@ file_obj.close()
 #find content and replace it
 found = False
 for i in range(len(lines)):
-    if CONTENT in lines[i]:
+    if CONTENT in lines[i] and not lines[i].startswith(COMMENT_TAG):
         lines[i] = NEW_CONTENT
         found = True
         break
